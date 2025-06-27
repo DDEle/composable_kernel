a57118b5e67edbd8d9bb42115f55c8f5bbeb7961
Ding, Yi <yi.ding@amd.com>
2025-06-27 12:17:47 +0000

    debug

--- include/ck_tile/core/config.hpp
+++ include/ck_tile/core/config.hpp
@@ -254,7 +254,7 @@
 #endif
 
 #ifndef CK_TILE_USE_BUFFER_ADDRESSING_BUILTIN
-#if __clang_major__ == 20
+#if __clang_major__ == 20 && 0
 #define CK_TILE_USE_BUFFER_ADDRESSING_BUILTIN 1
 #else
 #define CK_TILE_USE_BUFFER_ADDRESSING_BUILTIN 0
