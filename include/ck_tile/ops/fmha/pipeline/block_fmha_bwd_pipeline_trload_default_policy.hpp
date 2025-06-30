bd5a63caf5f53443bc9f5d9391c9b02ed3f592e8
Ding, Yi <yi.ding@amd.com>
2025-06-30 05:06:55 +0000

    add prefetch

--- include/ck_tile/ops/fmha/pipeline/block_fmha_bwd_pipeline_trload_default_policy.hpp
+++ include/ck_tile/ops/fmha/pipeline/block_fmha_bwd_pipeline_trload_default_policy.hpp
@@ -1112,8 +1112,9 @@ struct BlockFmhaBwdPipelineTrLoadDefaultPolicy
         constexpr index_t smem_size_bias = GetSmemSizeBias<Problem>();
 
         constexpr index_t smem_size_stage0 = smem_size_k + smem_size_v;
-        constexpr index_t smem_size_stage1 = smem_size_q + smem_size_do + smem_size_lse +
+        constexpr index_t smem_size_stage1 = smem_size_q * 2 + smem_size_do * 2 + smem_size_lse +
                                              smem_size_d + max(smem_size_bias, smem_size_ds);
+        CK_PRINT<smem_size_stage0, smem_size_stage1>();
 
         return max(smem_size_stage0, smem_size_stage1);
     }
