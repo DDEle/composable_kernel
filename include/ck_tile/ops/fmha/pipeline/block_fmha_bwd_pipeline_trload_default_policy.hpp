99c10fba8c07ccc8b3105e7ebc77fc9dc814bfe5
Ding, Yi <yi.ding@amd.com>
2025-10-28 02:37:08 +0000

    wip

--- include/ck_tile/ops/fmha/pipeline/block_fmha_bwd_pipeline_trload_default_policy.hpp
+++ include/ck_tile/ops/fmha/pipeline/block_fmha_bwd_pipeline_trload_default_policy.hpp
@@ -1149,15 +1149,51 @@ struct BlockFmhaBwdPipelineTrLoadDefaultPolicy
             constexpr index_t MFMA_INST     = Gemm0MFMA;
             constexpr index_t LDS_READ_INST = OGradT_LDS_READ + LSE_LDS_READ + D_LDS_READ;
 
-            constexpr index_t lcm_inst = lcm(VMEM_READ_INST, MFMA_INST, LDS_READ_INST);
-            static_for<0, lcm_inst, 1>{}([&](auto i) {
-                if constexpr(i % (lcm_inst / VMEM_READ_INST) == 0)
-                    __builtin_amdgcn_sched_group_barrier(0x020, 1, 0); // VMEM read
-                if constexpr(i % (lcm_inst / MFMA_INST) == 0)
-                    __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
-                if constexpr(i % (lcm_inst / LDS_READ_INST) == 0)
-                    __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
-            });
+            // constexpr index_t lcm_inst = lcm(VMEM_READ_INST, MFMA_INST, LDS_READ_INST);
+            // static_for<0, lcm_inst, 1>{}([&](auto i) {
+            //     if constexpr(i % (lcm_inst / VMEM_READ_INST) == 0)
+            //         __builtin_amdgcn_sched_group_barrier(0x020, 1, 0); // VMEM read
+            //     if constexpr(i % (lcm_inst / MFMA_INST) == 0)
+            //         __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            //     if constexpr(i % (lcm_inst / LDS_READ_INST) == 0)
+            //         __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            // });
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
+            __builtin_amdgcn_sched_group_barrier(0x008, 1, 0); // MFMA
+            __builtin_amdgcn_sched_group_barrier(0x100, 1, 0); // DS read
         }
 
         CK_TILE_DEVICE static constexpr void SchedulerGemm12()
