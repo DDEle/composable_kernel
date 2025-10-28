f97d282471817b57743bd39447a2416599cbcdd1
Ding, Yi <yi.ding@amd.com>
2025-10-28 06:40:23 +0000

    dbg

--- include/ck_tile/ops/flatmm/pipeline/flatmm_pipeline_agmem_bgmem_creg_v1.hpp
+++ include/ck_tile/ops/flatmm/pipeline/flatmm_pipeline_agmem_bgmem_creg_v1.hpp
@@ -34,8 +34,8 @@ struct BaseFlatmmPipelineAGmemBGmemCRegV1
         }
         else if(TailNumber::Odd == tail_num)
         {
-            return run_func(bool_constant<true>{},
-                            integral_constant<TailNumber, TailNumber::Odd>{});
+            // return run_func(bool_constant<true>{},
+            //                 integral_constant<TailNumber, TailNumber::Odd>{});
         }
         throw std::runtime_error("Error: Unsupported TailNumber!");
         // return run_func(bool_constant<true>{}, integral_constant<TailNumber,
