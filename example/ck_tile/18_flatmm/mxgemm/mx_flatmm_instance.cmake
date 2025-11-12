f4d3e2eef75bb0def79927e4d34a5ad0467fbe3a
Ding, Yi <yi.ding@amd.com>
2025-11-12 03:29:45 +0000

    fp8-wip

--- example/ck_tile/18_flatmm/mxgemm/mx_flatmm_instance.cmake
+++ example/ck_tile/18_flatmm/mxgemm/mx_flatmm_instance.cmake
@@ -1,7 +1,5 @@
 function(mx_flatmm_instance_generate FILE_LIST)
     set(FLATMM_CONFIG MXfp4_FlatmmConfig16)
-    set(A_DATA_TYPE FP4)
-    set(B_DATA_TYPE FP4)
     set(C_DATA_TYPE FP16)
     set(A_LAYOUT ROW)
     set(B_LAYOUT COL)
@@ -10,15 +8,21 @@ function(mx_flatmm_instance_generate FILE_LIST)
     # foreach(PERSISTENT false true)
     # TODO: Persistent kernels are disabled due to compilation failures with some LLVM versions.  
     foreach(PERSISTENT false)
-        foreach(SPLIT_K false true)
-            foreach(HAS_HOT_LOOP false true)
-                foreach(TAIL_NUMBER ODD EVEN)
-                    set(KERNEL_FILE mxgemm/mx_flatmm_instance_${PERSISTENT}_${SPLIT_K}_${HAS_HOT_LOOP}_${TAIL_NUMBER}.cpp)
-                    configure_file(
-                        ${CMAKE_CURRENT_SOURCE_DIR}/mxgemm/mx_flatmm_instance.cpp.in
-                        ${CMAKE_CURRENT_BINARY_DIR}/${KERNEL_FILE}
-                        @ONLY)
-                    list(APPEND ${FILE_LIST} ${CMAKE_CURRENT_BINARY_DIR}/${KERNEL_FILE})
+        foreach(DATA_TYPE FP4 FP8)
+        # foreach(DATA_TYPE FP4)
+            set(A_DATA_TYPE ${DATA_TYPE})
+            set(B_DATA_TYPE ${DATA_TYPE})
+            foreach(SPLIT_K false true)
+                foreach(HAS_HOT_LOOP false true)
+                    foreach(TAIL_NUMBER ODD EVEN)
+                        set(KERNEL_FILE mxgemm/mx_flatmm_instance_${PERSISTENT}_${DATA_TYPE}_${SPLIT_K}_${HAS_HOT_LOOP}_${TAIL_NUMBER}.cpp)
+                        string(TOLOWER ${KERNEL_FILE} KERNEL_FILE)
+                        configure_file(
+                            ${CMAKE_CURRENT_SOURCE_DIR}/mxgemm/mx_flatmm_instance.cpp.in
+                            ${CMAKE_CURRENT_BINARY_DIR}/${KERNEL_FILE}
+                            @ONLY)
+                        list(APPEND ${FILE_LIST} ${CMAKE_CURRENT_BINARY_DIR}/${KERNEL_FILE})
+                    endforeach()
                 endforeach()
             endforeach()
         endforeach()
