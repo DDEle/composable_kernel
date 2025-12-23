258fbfa9cfcfdddf40004174bf18d8957b4e7721
Ding, Yi <yi.ding@amd.com>
2025-12-23 03:02:43 +0000

    small tile

--- example/ck_tile/18_flatmm/mxgemm/mx_flatmm.hpp
+++ example/ck_tile/18_flatmm/mxgemm/mx_flatmm.hpp
@@ -14,8 +14,8 @@
 // GEMM config with 16x16 warp tile
 struct MXfp4_FlatmmConfig16
 {
-    static constexpr ck_tile::index_t M_Tile = 128;
-    static constexpr ck_tile::index_t N_Tile = 512;
+    static constexpr ck_tile::index_t M_Tile = 32;
+    static constexpr ck_tile::index_t N_Tile = 128;
     static constexpr ck_tile::index_t K_Tile = 256;
 
     static constexpr ck_tile::index_t M_Warp = 1;
